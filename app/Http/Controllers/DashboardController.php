d00027c9d8b4fd45f4c520a0d1c130f0103ec353
Duey12 <duwaine.gisf1@gmail.com>
2021-09-26 17:48:36 -0500

    Add lewis file to try solve merge conflict

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
 class DashboardController extends Controller
 {
@@ -18,4 +18,12 @@ class DashboardController extends Controller
     public function admin_index(){
       return view("admin.index");
 }
+  public function dash(){
+    if(Auth::User()->is_admin =='0'){
+        return redirect()->route('Dashboard');
+    }
+    else{
+        return redirect()->route('AdminDashboard');
+    }
+  }
 }
